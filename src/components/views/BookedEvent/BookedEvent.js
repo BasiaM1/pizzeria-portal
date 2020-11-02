23c3a3785965c1c55d60add9d08b3f1db20d522d
Basia <b.murzynowska@wp.pl>
2020-11-02 11:07:49 +0100

    Update BookedEvent.js
    Fix param id in event

--- src/components/views/BookedEvent/BookedEvent.js
+++ src/components/views/BookedEvent/BookedEvent.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './BookedEvent.module.scss';
+import { useParams } from 'react-router-dom';
 
-const BookedEvent = ({ eventId }) => (
+const BookedEvent = () => {
+  let { eventId } = useParams();
+  
+  return(
   <div className={styles.component}>
     <h2>BookedEvent view</h2>
     {eventId}
   </div>
-);
-
-BookedEvent.propTypes = {
-  eventId: PropTypes.string,
+  );
 };
-
-
 export default BookedEvent;
