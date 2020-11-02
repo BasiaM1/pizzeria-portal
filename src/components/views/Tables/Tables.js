123ec42623089c44638d02bd31d138d32fdc956b
Basia <b.murzynowska@wp.pl>
2020-11-02 11:12:17 +0100

    Update Tables.js

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 const Tables = () => (
   <div className={styles.component}>
     <h2>Tables view</h2>
-    <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`} activeClassName='active'>New booking table</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/booking/123abc`} activeClassName='active'>Booked table 123abc</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/event/new`} activeClassName='active'>New booking event</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/event/456abc`} activeClassName='active'>Booked event 456abc</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`}>New booking table</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/123abc`}>Booked table 123abc</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/event/new`}>New booking event</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/event/456abc`}>Booked event 456abc</Link>
   </div>
 );
 
